0927e873c75cfbbbd5ad3d666cd0212ba915aa50
Kimson Wong <kimsonwong@gmail.com>
2019-01-07 12:17:14 +0800

    added carrierwave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+
+    if Rails.env.development? || Rails.env.test?
+        CarrierWave.configure do |config|
+            config.storage = :file
+        end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_152838) do
+ActiveRecord::Schema.define(version: 2019_01_07_040425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "file"
+    t.string "caption"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_01_05_152838) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "users"
 end
